3cc659a7aec031235668ff2470d76c053fcaac80
Marcelo Cristian <marcelo.cristian.969@gmail.com>
2021-11-19 00:51:58 -0300

    contador componentizado

---
+++ fundamentos-react/src/components/contador/Botoes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Botoes(props) {
+  return (
+    <div>
+      <button onClick={props.setInc}>+</button>
+      <button onClick={props.setDec}>-</button>
+    </div>
+  );
+}
