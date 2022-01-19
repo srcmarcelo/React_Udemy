943c58fda087d3a448046ef732527e6cb33d1b97
Marcelo Cristian <marcelo.cristian.969@gmail.com>
2022-01-19 00:28:30 -0300

    useReducer 01 - aula 74

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useReducer } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
-const UseReducer = (props) => {
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-        </div>
-    )
+const initialState = {
+  cart: [],
+  products: [],
+  user: null,
+  number: 0,
+  other: "...",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "numberAdd2":
+      return { ...state, number: state.number + 2 };
+    case "login":
+      return { ...state, user: { name: action.payload } };
+    default:
+      return state;
+  }
 }
 
-export default UseReducer
+const UseReducer = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+
+      <div className="center">
+        {state.user ? (
+          <span className="text">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem usuario</span>
+        )}
+        <span className="text">{state.number}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "login", payload: "Evodia" })}
+          >
+            login
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberAdd2" })}
+          >
+            +2
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseReducer;
