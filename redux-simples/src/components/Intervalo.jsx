bebade1bdc439166b7773f9a121cf2210b4d5c7f
Marcelo Cristian <marcelo.cristian.969@gmail.com>
2022-01-25 23:58:50 -0300

    finalizando projeto redux-simples

--- redux-simples/src/components/Intervalo.jsx
+++ redux-simples/src/components/Intervalo.jsx
@@ -1,21 +1,59 @@
 import React from "react";
 import "./Intervalo.css";
+import { connect } from "react-redux";
 
 import Card from "./Card";
+import {
+  alterarNumeroMinimo,
+  alterarNumeroMaximo,
+} from "../store/actions/numeros";
 
-export default function Intervalo(props) {
+function Intervalo(props) {
+  const { max, min } = props;
   return (
     <Card title="Intervalo de Numeros" red>
       <div className="Intervalo">
         <span>
           <strong>Minimo:</strong>
-          <input type="number" value={0} readOnly />
+          <input
+            type="number"
+            value={min}
+            onChange={(e) => props.alterarMinimo(+e.target.value)}
+          />
         </span>
         <span>
           <strong>Minimo:</strong>
-          <input type="number" value={10} readOnly />
+          <input
+            type="number"
+            value={max}
+            onChange={(e) => props.alterarMaximo(+e.target.value)}
+          />
         </span>
       </div>
     </Card>
   );
 }
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    alterarMinimo(novoNumero) {
+      // Action Creator -> action
+      const action = alterarNumeroMinimo(novoNumero);
+      dispatch(action);
+    },
+    alterarMaximo(novoNumero) {
+      // Action Creator -> action
+      const action = alterarNumeroMaximo(novoNumero);
+      dispatch(action);
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo);
